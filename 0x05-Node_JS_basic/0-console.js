f54ff4de91ae683275e0056fa9d6437fd7fe7c6f
Kayode <136080928+KDEE247@users.noreply.github.com>
2024-07-22 09:45:59 +0100

    0-console.js...

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Kayode Ogidan <https://github.com/KDEE247>
  */
 const displayMessage = (msg) => {
   console.log(msg);

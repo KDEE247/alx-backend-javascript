41cf5634fb1c86273f37669f9953f9e7fa8a93e0
Kayode <136080928+KDEE247@users.noreply.github.com>
2024-07-22 09:46:51 +0100

    2-read_file.js...

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Kayode Ogidan <https://github.com/KDEE247>
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
